fb6193d415a30a6b47e5660cf269be6850a3d117
wbagdon <wbagdon@gmail.com>
2015-06-03 17:18:27 -0400

    Update to fit Python Style Guide

--- media.py
+++ media.py
@@ -1,5 +1,5 @@
 class Movie:
-	#constructor requiring moviews to have title, poster image url and youtube trailer url
+	# constructor requiring moviews to have title, poster image url and youtube trailer url
 	def __init__(self, title, poster_image_url, trailer_youtube_url, release_date, tag_line):
 		self.title = title
 		self.poster_image_url = poster_image_url
